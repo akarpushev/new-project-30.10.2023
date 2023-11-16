4b7e930d329282d6d6d7105a8f7a3c113527a679
Алексей <akarpushev@yandex.ru>
2023-11-16 21:04:58 +0300

    добавил файлы

--- test6.py
+++ test6.py
@@ -1,3 +1,22 @@
 dict1 = {'kirill': 89112345544, 'ivan': 89112343321, 'oleg': 89765432132}
-name = input('Введите имя: ')
-print(f'Телефон {name}:', dict1[name])
+#try:
+    #name = input('Введите имя: ')
+    #print(f'Телефон {name}: {dict1[name]}')
+#except KeyError;
+    #print('Имени нет')
+dict2 = {
+    'fedor': 98234599988,
+    'kirill': 1111111111
+}
+dict1.update(dict2)
+
+dict1['viktor'] = 9998887765
+print(dict1)
+
+
+name = input('Введите имя: ').lower()
+print(f'Телефон абонента {name} - {dict1.get(name,"не найден")}')
+print(dict1.items())
+print(dict1.values())
+print(dict1.keys())
+
