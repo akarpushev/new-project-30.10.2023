c1bd19d7064a5a1defccf0215ca7d0ffef751d70
Алексей <akarpushev@yandex.ru>
2023-11-20 01:53:16 +0300

    homework не работает

--- homework.py
+++ homework.py
@@ -1,3 +1,22 @@
+import pymysql
+connection = pymysql.connect(host='nadejnei.net', user='student', password='1q2w#E$R', database='test', port=33306)
+cursor = connection.cursor()
+cursor.execute('select * from telsprav')
+data = cursor.fetchall()
+
+for elem in data:
+    name = elem[0]
+    surname = elem[1]
+    sex = elem[2]
+    phone = elem[3]
+    users = (f'{name}, {surname}, {sex}, {phone}').split()
+
 while True:
-    name = input("Введите имя: ")
-    print(name)
+    username = input("Введите имя: ")
+    #for name in users[0]:
+    if username in users[0]:
+        #if username == users[0]:
+        print('Телефон абонента', username, users[3])
+        break
+    print('Абонент', username, 'отсутствует')
+    continue
