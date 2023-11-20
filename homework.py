3ea9cd5cfe16481553e48505426c0437875b4319
Алексей <akarpushev@yandex.ru>
2023-11-20 19:35:50 +0300

    окончательный вариант

--- homework.py
+++ homework.py
@@ -1,23 +1,27 @@
 import pymysql
 connection = pymysql.connect(host='nadejnei.net', user='student', password='1q2w#E$R', database='test', port=33306)
 cursor = connection.cursor()
-cursor.execute('select * from telsprav')
+cursor.execute('select name, phone from telsprav')
 data = cursor.fetchall()
+connection.close()
 
-users = []
+users = {}
 for elem in data:
-    name = elem[0]
-    surname = elem[1]
-    sex = elem[2]
-    phone = elem[3]
-    users += [(f'{name} {surname} {sex} {phone}').split()]
-#print(users)
+    users[elem[0]] = elem[1]
+print(users)
 
 while True:
-    username = input("Введите имя: ")
-    for name in users:
-        if name != username:
-            print('Телефон абонента', username, users[3])
+    username = input('Введите имя абонента: ').capitalize()
+    phone = users.get(username, 'не найден')
+    print(f'Телефон абонента {username} - {phone}')
 
-    print('Абонент', username, 'отсутствует')
+
+
+
+    # for name, phone in users.items():
+    #     if username == name:
+    #         print('Телефон абонента', username, phone)
+    #         break
+    #
+    #     print('Абонент', username, 'отсутствует')
 
